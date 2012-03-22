0961c9a085d8ee8315e6fd8d31a9346a199258a9
Kota Mizushima <mizukota@gmail.com>
2012-03-22 11:59:35 +0900

    Implement encrypt method.

--- src/main/scala/com/ubiregi/api/security/package.scala
+++ src/main/scala/com/ubiregi/api/security/package.scala
@@ -4,15 +4,17 @@ import java.security.SecureRandom
 import java.security.MessageDigest
 
 package object security {
-  def encrypt(secret: String): (Array[Byte], String) = {
+  private final val SEPARATOR = ":"
+  def encrypt(secret: String): (String, String) = {
     def generateSalt(): String = {
       val random = SecureRandom.getInstance("SHA1PRNG");
       random.setSeed(System.currentTimeMillis())
-      "%x".format(random.nextInt())
+      "%08x".format(random.nextInt())
     }
     val salt = generateSalt()
     val digester = MessageDigest.getInstance("SHA")
-    digester.update((salt + secret).getBytes())
-    (digester.digest(), salt)
+    digester.update((salt + SEPARATOR + secret).getBytes())
+    val base64Encoded = new BASE64Encoder().encode(digester.digest())
+    (base64Encoded, salt)
   }
 }
