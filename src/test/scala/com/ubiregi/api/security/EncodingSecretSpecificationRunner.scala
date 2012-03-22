77d8d580604868562e9eb24f9ca76d8a7386c0d2
Kota Mizushima <mizukota@gmail.com>
2012-03-22 19:39:44 +0900

    typo orz

--- src/test/scala/com/ubiregi/api/security/EncodingSecretSpecificationRunner.scala
+++ src/test/scala/com/ubiregi/api/security/EncodingSecretSpecificationRunner.scala
@@ -4,6 +4,6 @@ import specs2.run
 
 object EncodingSecretSpecificationRunner extends App {
   run.main(Array(
-    "com.ubiregi.api.security.EncryptSpecification"
+    "com.ubiregi.api.security.EncodingSecretSpecification"
   ))
 }
